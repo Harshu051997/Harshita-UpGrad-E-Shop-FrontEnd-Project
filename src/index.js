2bf6b2e03cf01e3bf51beec33c5e94a033332fff
Harshu051997 <shrivastava.harshi.15@gmail.com>
2024-03-11 21:03:11 +0530

    error fixed

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Navbar from './common/navbar/Navbar';
 import { Provider } from 'react-redux';
 import Store from './redux/store/Store';
 import { BrowserRouter } from 'react-router-dom';
-const  = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
    <React.StrictMode>
 
